0e288cd0c073a90d4fe75257ef12907fb2990cdc
anshul45 <anshulgarwal45@gmail.com>
2023-07-30 22:31:35 +0530

    create app

---
+++ create_react_app/app.js
@@ -0,0 +1,6 @@
+
+       
+        const react_heading = React.createElement("h1",{null:null},"My First Heading");
+        const react_root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(react_heading);    
