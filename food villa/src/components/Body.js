3214317d50d5bb3e36ad0facd372b38a8867a25f
Anshul45 <anshulgarwal025@gmail.com>
2023-08-03 10:53:39 +0530

    added search functionality to our web_app

--- food villa/src/components/Body.js
+++ food villa/src/components/Body.js
@@ -2,21 +2,53 @@ import { restaurantList } from "../contants";
 
 import RestrantCard from "./RestrantCard";
 
+import { useState } from "react";
 
-
-
+function filterData(searchInput) {
+  const filterData = restaurantList.filter((restaurant) =>
+  restaurant.info.name.toLowerCase().includes(searchInput)
+  );
+  return filterData;
+}
 
 const Body = () => {
-    return(
-            <div className='restaurant-list'>
-                  {
-                    restaurantList.map((restaurant) =>{
-                            return <RestrantCard key={restaurant.info.resId} {...restaurant.info} />;
-                    })
-                  }  
-                   
-            </div>
-    )
-}
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchInput, setSearchInput] = useState("KFC");
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchInput}
+          onChange={(e) => setSearchInput( e.target.value.toLowerCase())}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            //filter data
+            const data = filterData(searchInput);
+
+            //update data
+
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+
+      <div className="restaurant-list">
+        {restaurants?.map((restaurant) => {
+          return (
+            <RestrantCard key={restaurant.info.resId} {...restaurant.info} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Body;
--- food villa/src/components/RestrantCard.js
+++ food villa/src/components/RestrantCard.js
@@ -3,9 +3,9 @@ const RestrantCard = ({image,name,cuisine,rating}) =>{
    
      return(
              <div className='card'>
-                     <img src={image.url}/>
+                     <img src={image?.url}/>
                      <h2>{name}</h2>
-                    <h3>{cuisine.map(data => {return data.name}).join(", ")}</h3> 
+                    <h3>{cuisine?.map(data => {return data.name}).join(", ")}</h3> 
                      <h4>{rating_text} stars</h4> 
              </div>
      )
